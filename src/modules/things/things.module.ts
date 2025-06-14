50a47b3d102b973fd5c089ab964f1ee24b5b03f0
Ako <akaki.vashakidze1@gmail.com>
2025-06-14 16:51:22 +0400

    jwt secret

--- src/modules/things/things.module.ts
+++ src/modules/things/things.module.ts
@@ -6,11 +6,17 @@ import { CatsService } from './services/cats.service';
 import MongooseModels from './models';import * as dotenv from 'dotenv'; 
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 dotenv.config();
 
 @Module({
     imports: [
+        JwtModule.register({
+            global: true,
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: '24h' },
+        }),
         MongooseModule.forRoot(process.env.DB_URL!),
         MongooseModule.forFeature(MongooseModels),
     ],
