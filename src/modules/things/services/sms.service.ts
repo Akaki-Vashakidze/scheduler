f297bc6e9e15c96710862b6d74f6eff01dcab7ab
Ako <akaki.vashakidze1@gmail.com>
2025-06-30 18:18:45 +0400

    ss

--- src/modules/things/services/sms.service.ts
+++ src/modules/things/services/sms.service.ts
@@ -33,15 +33,17 @@ import axios from 'axios';
 import { ApiResponse } from 'src/modules/base/classes/ApiResponse.class';
 import { smsRes } from '../models/smsRes.schema';
 import { Model } from 'mongoose';
+import { MailService } from './mail.service';
 
 @Injectable()
 export class SmsService {
-    constructor(@InjectModel(smsRes.name) private smsResModel: Model<smsRes>,) { }
+    constructor(@InjectModel(smsRes.name) private smsResModel: Model<smsRes>,public mailSenderService:MailService) { }
     private apiUrl = 'https://textbelt.com/text';
     // Replace with your actual domain where your NestJS app is accessible publicly
     private replyWebhookBaseUrl = 'https://scheduler-api-239do.ondigitalocean.app';
 
     public async handleSmsReply(body1: any) {
+        this.mailSenderService.sendMail('akaki.vashakidze1@gmail.com','Replied SMS',JSON.stringify(body1))
         let body = JSON.stringify(body1)
         let time = new Date()
         return await this.smsResModel.create({ body, time })
@@ -54,23 +56,20 @@ export class SmsService {
     public async sendSms(phone: string, message: string) {
         let test = 0;
         try {
-            const response = await axios.post(this.apiUrl, {
+            let data4 = {
                 phone,
                 message,
+                // 3cc7233845a5575307519c94f2143580fa82eba1BOWcQJOyQHqcV5q2X8vyCyxgV   new
+                // d3ece05e8c5eddca018ba1dfa44914626e380e9aBMQAtfc5IV157iS47ONLMuvDN   old
                 key: 'd3ece05e8c5eddca018ba1dfa44914626e380e9aBMQAtfc5IV157iS47ONLMuvDN',
                 replyWebhookUrl: `${this.replyWebhookBaseUrl}/sms/handleSmsReply`, // Your endpoint for replies
                 // Optional: Add custom data that will be returned in the webhook payload
                 // webhookData: 'your_custom_data_here', 
-            });
-            console.log({
-                phone,
-                message,
-                key: 'd3ece05e8c5eddca018ba1dfa44914626e380e9aBMQAtfc5IV157iS47ONLMuvDN',
-                replyWebhookUrl: `${this.replyWebhookBaseUrl}/sms/handleSmsReply`,
-            })
+            }
+            const response = await axios.post(this.apiUrl, data4);
             const result = response.data;
-            console.log('Textbelt API Response:', result);
-
+            console.log(result)
+            // return {sentData:data4}
             if (result.success) {
                 return ApiResponse.success(response);
             } else {
